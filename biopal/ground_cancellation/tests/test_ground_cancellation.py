f11a6c77d052a5d35e488e66030e2474d06285a3
Emanuele Giorgi <emanuele.giorgi@aresys.it>
2021-04-12 10:18:18 +0200

    added doc strings to the test

--- biopal/ground_cancellation/tests/test_ground_cancellation.py
+++ biopal/ground_cancellation/tests/test_ground_cancellation.py
@@ -1,39 +1,49 @@
 from biopal.ground_cancellation.ground_cancellation import ground_cancellation
 import numpy as np
+import warnings
 
 
-def initial_values():
-    Nrg = 378
-    Naz = 2656
-    data_dummy = 1/np.sqrt(2)*np.random.rand(Nrg,Naz)+ 1j/np.sqrt(2)*np.random.rand(Nrg,Naz)
-    pol_dict = {'hh': data_dummy, 'hv': data_dummy, 'vv': data_dummy }
-    data_input_dict = {'BSL_00': pol_dict, 'BSL_01': pol_dict, 'BSL_02': pol_dict }
+def test_ground_cancellation():
+    """
+    ground_cancellation functional tests
     
-    wave_number_dummy = np.ones((Nrg,Naz))
-    wave_number_dict = {'BSL_00': 0*wave_number_dummy, 'BSL_01': -0.03*wave_number_dummy, 'BSL_02': 0.08*wave_number_dummy }
+    Executes ground_cancellation APP twice:
+        
+        first time with space varying flag disabled and dummy inputs.
+        
+        second time with space varying flag enabled and dummy inputs: 
+               to enable it, the scalar enhanced forest height coming from 
+               the initial_values() function is converted to a 2D map.
+        
+    The generated output is tested to be a dictionary with the three expected 
+    polarizations, each containing a 2D map with expected Nrg, Naz dimensions
+   
+    Test execution:
+    pytest needs to be installed in the environment
+    from command line, go in the BioPAL directory and type the following instruction:
+        
+        >>>pytest
+        
+    This will automatically executes all the pytest formatted tests found in BioPAL
     
-    multi_master_flag = True
-    enhanced_forest_height = 50
-    equalization_flag = '1'
-    resolution_m_slant_rg = 25
-    off_nadir_angle = 0.5*np.ones((Nrg,Naz))
-    slope = 0.05*np.ones((Nrg,Naz))
+    Parameters
+    ----------
+    None, all the dummy parameters are loaded from the initial_values() function
     
-    return (
-        Nrg, 
-        Naz,            
-        data_input_dict,
-        wave_number_dict,
-        multi_master_flag,
-        enhanced_forest_height,
-        equalization_flag,
-        resolution_m_slant_rg,
-        off_nadir_angle,
-        slope,
-        )
+    """
     
+    warnings.filterwarnings('ignore') # warnings silenced for convenience
+   
+    ground_cancellation_1()
+    ground_cancellation_2()
     
-def test_ground_cancellation_1():
+
+def ground_cancellation_1():
+    """
+    ground_cancellation functional test #1
+    Executes ground_cancellation APP, with space varying flag disabled and 
+    dummy inputs
+    """
     
     (
      Nrg, 
@@ -67,7 +77,12 @@ def test_ground_cancellation_1():
         assert data.shape==(Nrg,Naz), 'wrong output shape, in test with space varying OFF'
 
 
-def test_ground_cancellation_2():
+def ground_cancellation_2():
+    """
+    ground_cancellation functional test #2
+    Executes ground_cancellation APP, with space varying flag enabled and 
+    dummy inputs
+    """
     
     (
       Nrg, 
@@ -82,10 +97,10 @@ def test_ground_cancellation_2():
       slope,
       ) = initial_values()
     
+    # converto to 2dMap, tio enable the space varying
     enhanced_forest_height_map = slope.copy()
     enhanced_forest_height_map.fill(enhanced_forest_height_scalar)
-
-           
+        
     notched_pol_dict = ground_cancellation(
         data_input_dict,
         wave_number_dict,
@@ -103,3 +118,64 @@ def test_ground_cancellation_2():
     for pol, data in notched_pol_dict.items():
         
         assert data.shape==(Nrg,Naz), 'wrong output shape, in test with space varying ON'
+        
+        
+def initial_values():
+    """
+    Generates dummy variables for the ground cancellation APP, for functional testing
+    
+    Returns
+    -------
+    Nrg
+        integer, number of range samples for the dummy maps
+    Naz
+        integer, number of azimuth lines for the dummy maps
+    data_input_dict
+        dictionary with three acquisitions, each containing a sub dictionary with 
+        three polarizations, each containing  a raster with random complex values
+    wave_number_dict
+        dictionary with three acquisitions, each containing a raster with 
+        constant dummy real values
+    multi_master_flag
+        boolean (see ground cancellation documentation)
+    enhanced_forest_height
+            positive number (see ground cancellation documentation)
+    equalization_flag
+        string, enumeration flag (see ground cancellation documentation)
+    resolution_m_slant_rg
+       positive number, slant range resolutuon 
+    off_nadir_angle
+        map with dummy constant values for off nadir angle (supposed radiants)
+    slope
+        map with dummy constant values for slope angle (supposed radiants)
+    """
+    
+    Nrg = 378
+    Naz = 2656
+    data_dummy = 1/np.sqrt(2)*np.random.rand(Nrg,Naz)+ 1j/np.sqrt(2)*np.random.rand(Nrg,Naz)
+    pol_dict = {'hh': data_dummy, 'hv': data_dummy, 'vv': data_dummy }
+    data_input_dict = {'BSL_00': pol_dict, 'BSL_01': pol_dict, 'BSL_02': pol_dict }
+    
+    wave_number_dummy = np.ones((Nrg,Naz))
+    wave_number_dict = {'BSL_00': 0*wave_number_dummy, 'BSL_01': -0.03*wave_number_dummy, 'BSL_02': 0.08*wave_number_dummy }
+    
+    multi_master_flag = True
+    enhanced_forest_height = 50
+    equalization_flag = '1'
+    resolution_m_slant_rg = 25
+    off_nadir_angle = 0.5*np.ones((Nrg,Naz))
+    slope = 0.05*np.ones((Nrg,Naz))
+    
+    return (
+        Nrg, 
+        Naz,            
+        data_input_dict,
+        wave_number_dict,
+        multi_master_flag,
+        enhanced_forest_height,
+        equalization_flag,
+        resolution_m_slant_rg,
+        off_nadir_angle,
+        slope,
+        )        
+        
